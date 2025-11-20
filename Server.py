3f8fe5937aa81d35df3adfbdf7c538fda8681867
Adrian Lizarazu <adrianlizarazu64@gmail.com>
2025-11-19 23:32:38 -0400

    Muestra

---
+++ Server.py
@@ -0,0 +1,10 @@
+# import mysql.connector
+
+# Conexión al servidor MySQL
+#conexion = mysql.connector.connect(
+   # host="localhost",
+    #user="tu_usuario",
+   # password="tu_contraseña"
+#)
+
+#cursor = conexion.cursor()

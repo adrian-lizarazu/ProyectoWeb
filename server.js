cd8cd551d0b3ea1555ef6c9485aa436727a5c900
Adrian Lizarazu <adrianlizarazu64@gmail.com>
2025-12-09 20:49:40 -0400

    implementacion HTTP2 y HTTP 2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+   
 
 const app = express();
 app.use(express.json());
@@ -29,3 +30,5 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Servidor corriendo en puerto 3000");
 });
+
+
